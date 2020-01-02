148de445637368037577bc260c1c92d9e0f89c97
DanielPantea <pantea.daniel6@gmail.com>
2020-01-02 14:46:51 +0200

    Am facut structuri pt profi si sali

--- FacultateApp/dani.c
+++ FacultateApp/dani.c
@@ -6,3 +6,28 @@ struct student
     int an;
     float taxa, nota;
 };
+struct sala
+{
+    char nume[11];
+    char tip;///c,s, sau l
+    int nrLoc;
+};
+void Read_Sali(struct sala sali[], int *nr, char finput[])
+{
+    FILE *fr = fopen(finput, "r");
+    char line[101], *p;
+    int k=0;
+    while(fgets(line,100,fr))
+    {
+        p=strtok(line, ",");
+        strcpy(sali[k].nume,p);
+        p=strtok(NULL, ",");
+        sali[k].nrLoc = atoi(p);
+        p=strtok(NULL, ",");
+        sali[k].tip = *p;
+        p=strtok(NULL, ",");
+        k++;
+    }
+    *nr=k;
+
+}
--- FacultateApp/main.c
+++ FacultateApp/main.c
@@ -15,16 +15,47 @@ struct student
     float taxa, nota;
 }students[301];
 
+struct profesor
+{
+    char nume[31], prenume[31], materii[5][31];
+    int rol[31][3];///linia reprezinta materia, coloana: 0-Curs, 1-Seminar, 2-Laborator, daca rol[i]=1 atunci profesorul indeplineste rolul i
+    int nrOre[5],nrMaterii;
+    struct preferinta
+    {
+        int ora;
+        char sala[11];
+    }pref[5];
+}profesors[101];
+
+struct sala
+{
+    char nume[11];
+    char tip;///c,s, sau l
+    int nrLoc;
+}sali[101];
+struct materii
+{
+    char nume[31];
+    int an, nrProfi;
+    int profs[31];///retine doar indicii profesorilor
+};
+
 int main()
 {
 
-    int i, nrStud;
+    int i, nrStud, nrSali;
 
-    Read_Students(students,&nrStud,"students_input.csv");
+    Read_Students(students,&nrStud,"InputStudents.csv");
+    Read_Sali(sali, &nrSali,"InputSali.csv");
 
     for(i=0;i<nrStud;i++)
     {
         printf("%s, %s, %d, %s\n",students[i].nume,students[i].prenume,students[i].an,students[i].sectie);
+    }
+    printf("\n");
+    for(i=0;i<nrSali;i++)
+    {
+        printf("%s, %d, %c\n",sali[i].nume,sali[i].nrLoc,sali[i].tip);
     }
 
     return 0;
