146c2480367a02980f449c6e9c896c35706ca9ae
Cosmin-Albert <albertcosmin2000@gmail.com>
2020-01-26 14:38:13 +0200

    c2
    aa

--- FacultateApp/cosmin.c
+++ FacultateApp/cosmin.c
@@ -129,34 +129,3 @@ void Read_Students(struct student students[], int *nr, char finput[])
     *nr=k;
 }
 
-int main ()
-{int i,nrs,nrm,nrStud;
-   Read_Sali(sala,&nrs,"sali.in.txt");
-   Read_Materi(materie,&nrm,"materi.in.txt");
-   for(i=0;i<nrs;i++)
-   {
-       printf("%s, %d, %s\n",sala[i].numes,sala[i].tip_sala,sala[i].nr_locuri);
-   }
-
-    for(i=0;i<nrm;i++)
-   {
-       printf("%s, %d, %d,%s\n",materie[m].numeM,materie[m].anStudi,materie[m].nr_profesori,materie[m].numeP[i],materie[m].prenumeP[i];
-   }
-
-    Read_Students(students,&nrStud,"student.int");
-    for(i=0;i<nrStud;i++)
-    {
-        printf("%s, %s, %d, %s\n",students[i].nume,students[i].prenume,students[i].an,students[i].sectie);
-    }
-
-  char numE,prenumE;
-float nota_noua;
-int indice_materie
-    struct student v[200];
-    int n,m;
-    char materii[31][51];
-    inserare_nota(v,n,materii,m);
-    schimbare_nota(v,numE,prenumE,nota_noua,indice_materie);
-
-    return 0;
-}
